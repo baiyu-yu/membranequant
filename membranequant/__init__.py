68b7af8fda4d23cb72d14a791ecdfe09a54c07bf
baiyu-yu <135424680+baiyu-yu@users.noreply.github.com>
2026-07-20 23:54:49 +0800

    fix

--- membranequant/__init__.py
+++ membranequant/__init__.py
@@ -8,5 +8,9 @@ warnings.filterwarnings("ignore", category=FutureWarning, module=".*dualcellquan
 warnings.filterwarnings("ignore", message=".*remove_small_objects.*")
 warnings.filterwarnings("ignore", message=".*binary_opening.*")
 
+# Ignore PyTorch sparse tensor warning in cellpose dynamics
+warnings.filterwarnings("ignore", category=UserWarning, module=".*cellpose.*")
+warnings.filterwarnings("ignore", message=".*Sparse invariant checks.*")
+
 __version__ = "0.1.0"
 
